4f8468768cf5e124ca1301e33bbbb51ed82d9acc
marprintezi <marprintezi@gmail.com>
2024-05-24 12:10:08 +0300

    Update csv_to_sql_insert_data_format.py

--- GENERATED-DATA/csv_to_sql_insert_data_format.py
+++ GENERATED-DATA/csv_to_sql_insert_data_format.py
@@ -1,1 +1,6 @@
-C:\Users\marpr\insert_cook_data.sql;
+--run command in cmd
+python generate_cook.py;
+
+
+--run command in mariadb database cook_competition
+source C:\Users\marpr\insert_cook_data.sql;
